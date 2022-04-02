f349952bae69b0274373c2b5487c03956c5175cc
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-04-01 22:16:58 -0300

    filter, map e reduce

---
+++ topicos_js/map-filter-reduce/filter.js
@@ -0,0 +1,11 @@
+function filtraPares(arr) {
+    if (!arr || !arr.lengh) return;
+
+
+    const filteredArr = arr.filter((item)) => item  % === 0);
+
+    return filteredArr;
+
+}
+
+console.log(filtraPares([1,2,4,5,6,7,8,9]))
